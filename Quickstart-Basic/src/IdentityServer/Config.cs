23980b5c61501534684deee85eb63245a70113bf
Matt Claycomb <matt@claycombfamily.us>
2020-07-18 08:32:44 -0500

    Add MVC Client

--- Quickstart-Basic/src/IdentityServer/Config.cs
+++ Quickstart-Basic/src/IdentityServer/Config.cs
@@ -4,6 +4,7 @@
 
 using IdentityServer4.Models;
 using System.Collections.Generic;
+using IdentityServer4;
 
 namespace IdentityServer
 {
@@ -12,7 +13,8 @@ namespace IdentityServer
         public static IEnumerable<IdentityResource> IdentityResources =>
             new IdentityResource[]
             { 
-                new IdentityResources.OpenId() 
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(), 
             };
 
         public static IEnumerable<ApiScope> ApiScopes =>
@@ -39,6 +41,28 @@ namespace IdentityServer
 
                     // scopes that client has access to
                     AllowedScopes = {"api1"}
+                },
+
+                new Client
+                {
+                    ClientId = "mvc",
+                    
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    RedirectUris = { "https://localhost:5002/signin-oidc"},
+
+                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
                 }
             };
     }
