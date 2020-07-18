dd811310a98f2777facda833caace635d4430932
Matt Claycomb <matt@claycombfamily.us>
2020-07-17 22:58:31 -0500

    Add Sample API

---
+++ src/Api/IdentityController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api
+{
+    [Route("identity")][Authorize]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(User.Claims.Select(c => new {c.Type, c.Value}));
+        }
+    }
+}
