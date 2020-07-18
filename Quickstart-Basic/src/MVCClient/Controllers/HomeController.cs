2c147ae82022635e56acdf4355d04f53d9be527c
Matt Claycomb <matt@claycombfamily.us>
2020-07-18 10:59:01 -0500

    Add API Access to MVC Client

--- Quickstart-Basic/src/MVCClient/Controllers/HomeController.cs
+++ Quickstart-Basic/src/MVCClient/Controllers/HomeController.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MVCClient.Models;
+using Newtonsoft.Json.Linq;
 
 namespace MVCClient.Controllers
 {
@@ -26,6 +30,17 @@ namespace MVCClient.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<IActionResult> CallApi()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var content = await client.GetStringAsync("https://localhost:6001/identity");
+
+            ViewBag.Json = JArray.Parse(content).ToString();
+            return View("json");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
